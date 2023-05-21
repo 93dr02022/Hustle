1862abd5f4be20d3722b8b4b9effeade9b967ae5
afuwape sunday <afuwapesunday12@gmail.com>
2023-05-21 22:57:03 +0100

    initial commit

---
+++ config/envato.php
@@ -0,0 +1,8 @@
+<?php
+
+    return [
+
+        'purchase_code' => 'ea342e61-7551-4038-9131-dabeddf10015',
+        'username'      => 'abdulrafiurabiu'
+
+    ];
---
+++ webpack.mix.js
@@ -0,0 +1,28 @@
+const mix = require("laravel-mix");
+require("laravel-mix-compress");
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel applications. By default, we are compiling the CSS
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js("resources/js/app.js", "public/js")
+    .vue()
+    .js("node_modules/select2/dist/js/select2.min.js", "public/js/select2.js")
+    .postCss("resources/css/mdi.css", "public/css")
+    .postCss(
+        "node_modules/select2/dist/css/select2.min.css",
+        "public/css/select2.css"
+    )
+    .postCss("resources/css/app.css", "public/css", [require("tailwindcss")])
+    .version();
+mix.compress({
+    productionOnly: true,
+    minRatio: 1,
+});
