b71be6dc5eda39d4e400765a1eaaaeb02cc3427a
afuwape sunday <afuwapesunday12@gmail.com>
2023-06-07 17:13:24 +0100

    removed paypal from withdrawal settings WIP

--- app/Http/Validators/Main/Seller/Withdrawals/SettingsValidator.php
+++ app/Http/Validators/Main/Seller/Withdrawals/SettingsValidator.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Validator;
 
 class SettingsValidator
 {
-    
+
     /**
      * Validate form
      *
@@ -16,22 +16,18 @@ class SettingsValidator
     static function validate($request)
     {
         try {
-
-            // Set rules
-            $rules    = [
-                'paypal_email'  => 'nullable|email|max:60',
+            $rules = [
                 'offline_info'  => 'nullable',
             ];
 
             // Set errors messages
             $messages = [
-                'email.email'    => __('messages.t_validator_email'),
-                'email.max'      => __('messages.t_validator_max', ['max' => 60]),
+                'email.email' => __('messages.t_validator_email'),
+                'email.max' => __('messages.t_validator_max', ['max' => 60]),
             ];
 
             // Set data to validate
             $data     = [
-                'paypal_email' => $request->paypal_email,
                 'offline_info' => $request->offline_info
             ];
 
@@ -40,10 +36,8 @@ class SettingsValidator
 
             // Reset validation
             $request->resetValidation();
-
         } catch (\Throwable $th) {
             throw $th;
         }
     }
-
 }
