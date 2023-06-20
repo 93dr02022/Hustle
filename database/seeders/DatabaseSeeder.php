bbcac4188bb3f2c489db587c28358ecbb40e5143
afuwape sunday <afuwapesunday12@gmail.com>
2023-06-20 23:55:58 +0100

    gig and user seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -75,7 +75,8 @@ class DatabaseSeeder extends Seeder
         if (App::environment('local')) {
             $this->call([
                 UserSeeder::class,
-                StatesTableSeeder::class
+                StatesTableSeeder::class,
+                GigSeeder::class
             ]);
         }
     }
