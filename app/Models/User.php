dd8acc459190319ebd089aa235991f086b58aee2
westcoperations <richardejike0172@gmail.com>
2023-09-06 16:12:19 +0000

    sellers gigs showing on public profile

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,8 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Cache;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 
 class User extends Authenticatable
 {
@@ -264,4 +266,14 @@ class User extends Authenticatable
     {
         return $this->chat_contacts_to->merge($this->chat_contacts_from);
     }
+
+/**
+ * Get all of the Gigs for the User
+ *
+ * @return \Illuminate\Database\Eloquent\Relations\HasMany
+ */
+public function gigs(): HasMany
+{
+    return $this->hasMany(Gig::class, 'user_id');
+}
 }
