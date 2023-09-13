894421b6ad6fc35763fbcf8363484c7cce33bf14
westlove <87907449+westcorperations@users.noreply.github.com>
2023-09-13 14:45:06 +0100

    Update ReviewReceived.php

--- app/Notifications/User/Seller/ReviewReceived.php
+++ app/Notifications/User/Seller/ReviewReceived.php
@@ -44,6 +44,16 @@ class ReviewReceived extends Notification implements ShouldQueue
      */
     public function toMail($notifiable)
     {
+         // if there is app token proceed
+        if ($notifiable?->userNotificationSetting?->app_token) {
+            rescue(fn () => $this->toMobile($notifiable));
+        }
+
+        // if there is web token proceed
+        if ($notifiable?->userNotificationSetting?->notification_token) {
+            rescue(fn () => $this->toFirebase($notifiable));
+        }
+        
         // Set subject
         $subject = "[" . config('app.name') . "] " . __('messages.t_subject_seller_new_review');
 
